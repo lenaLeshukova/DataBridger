3dcbe36b5e2bdcb6623ab23998d9d36ff0a74c9e
Leshukova Elena <e-redina@ya.ru>
2026-03-13 19:13:03 +0300

    часть 1 курсовая

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
+import os
+
 from utils import load_operations_from_excel
 from views import main_dashboard
+
+# Получаем путь к папке, где лежит этот файл (src)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+# Поднимаемся на уровень выше в корень проекта и идем в data
+excel_path = os.path.join(base_dir, '..', 'data', 'operations.xlsx')
 
 
 def run():
     # Загружаем данные, если папка data лежит в корне проекта, Python её не видит, поднимаемся на уровень выше.
-    df = load_operations_from_excel('data/operations.xlsx')
+    df = load_operations_from_excel(excel_path)
 
     # Если df пустой или None, не идем дальше
     if df is None or df.empty:
         print("Критическая ошибка: Данные не загружены. Проверьте путь к файлу!")
         return
-
 
     # Передаем дату и DataFrame в основную функцию
     json_response = main_dashboard("2021-12-31 16:44:00", df)
